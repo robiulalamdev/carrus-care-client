f1ebf9d1f25c873c2b42beaaa9ba83aa8fba6c28
robiulalamdev <robiulalam762400@gmail.com>
2023-12-31 01:08:00 +0600

    chapta test

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,3 +2,5 @@
 // export const serverURL = "http://localhost:8000";
 export const baseQuery = "https://care-plus-server.onrender.com/api/v1";
 export const serverURL = "https://care-plus-server.onrender.com";
+
+export const chaptaKey = "6LfL3UApAAAAAL64SnP3eW_1NbywcPv7P3V-Bsqc";
--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -3,12 +3,21 @@ import RForm1 from "../../components/registration-ui/registrationFormStep1/RForm
 import RForm2 from "../../components/registration-ui/registrationFormStep2/RForm2";
 import RForm3 from "../../components/registration-ui/registrationFormStep3/RForm3";
 import Header from "../../components/shared/Header";
+import ReCAPTCHA from "react-google-recaptcha";
+import { chaptaKey } from "../../lib/api";
 
 const Registration = () => {
   const [step, setStep] = useState(1);
+  function onChange(value) {
+    console.log("Captcha value:", value);
+  }
 
   return (
     <>
+      <ReCAPTCHA
+        sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+        onChange={onChange}
+      />
       <div className="max-w-[1000px] mx-auto w-full py-8 px-2">
         <Header />
         <RForm1 step={step} setStep={setStep} show={true} />
