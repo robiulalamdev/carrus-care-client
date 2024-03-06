b13f9c05d138e61c1b5b5c0e6c72f899e13af3ef
robiulalamdev <robiulalam762400@gmail.com>
2024-03-06 23:33:30 +0600

    api update

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 // export const baseQuery = "http://localhost:8000/api/v1";
 // export const serverURL = "http://localhost:8000";
-export const baseQuery = "https://care-plus-server.onrender.com/api/v1";
-export const serverURL = "https://care-plus-server.onrender.com";
+export const baseQuery = "https://careplus-server.onrender.com/api/v1";
+export const serverURL = "https://careplus-server.onrender.com";
 
 export const chaptaKey = "6LfL3UApAAAAAL64SnP3eW_1NbywcPv7P3V-Bsqc";
