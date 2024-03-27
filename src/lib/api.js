b88f31ac32d2ba73d528058e2711141f1b612be3
robiulalamdev <robiulalam762400@gmail.com>
2024-03-27 12:18:00 +0600

    env file add

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,2 @@
-// export const baseQuery = "http://localhost:8000/api/v1";
-// export const serverURL = "http://localhost:8000";
-export const baseQuery = "https://careplus-server.onrender.com/api/v1";
-export const serverURL = "https://careplus-server.onrender.com";
-
-export const chaptaKey = "6LfL3UApAAAAAL64SnP3eW_1NbywcPv7P3V-Bsqc";
+/* eslint-disable no-undef */
+export const baseQuery = `${import.meta.env.VITE_BASE_URL}`;
